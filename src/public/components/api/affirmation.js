79286fe3887a6c20660a28e9b10f3b4ada648557
Dev <dev@borke.us>
2018-02-17 23:17:32 -0500

    Adding category for affirmations

--- src/public/components/api/affirmation.js
+++ src/public/components/api/affirmation.js
@@ -1,6 +1,12 @@
 function get_affirmation() {
   document.addEventListener('DOMContentLoaded', function () {
-    url = "https://api.fivecat.xyz/api/index.php/affirmation";
+    
+    var category = getQueryVariable('category');
+    if (category)
+      url = "https://api.fivecat.xyz/api/index.php/affirmation/random/" + category;
+    else
+      url = "https://api.fivecat.xyz/api/index.php/affirmation/random";
+      
     $.getJSON(url, function (json) {
       var affirmation = json.affirmation;
       var div = "<blockquote class='align-center'>" + affirmation + "</blockquote>";
